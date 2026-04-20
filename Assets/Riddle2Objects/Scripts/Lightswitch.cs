0347073f2382637d3190c3052c7f17ec6e8f6e7d
TelinoT <elias.piersig@icloud.com>
2026-04-20 16:20:34 +0200

    Finished object riddle
    - added a lightswitch
    - added text on the objects
    - finalized everything needed for the second riddle

---
+++ Assets/Riddle2Objects/Scripts/Lightswitch.cs
@@ -0,0 +1,101 @@
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.Interactables;
+using System.Collections;
+
+public class Lightswitch : XRSimpleInteractable
+{
+    public GameObject[] lights;
+
+    public GameObject[] appearingTexts;
+
+    public float dimmedLight = 0.5f;
+
+    private bool isOn = true;
+    
+    public float pushDistance = 0.05f;
+
+    private float[] originalIntensities;
+
+    private Vector3 originalPosition;
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        originalPosition = transform.localPosition;
+        
+        originalIntensities = new float[lights.Length];
+        
+        for (int i = 0; i < lights.Length; i++)
+        {
+            if (lights[i] != null)
+            {
+                Light lightComponent = lights[i].GetComponent<Light>();
+                if (lightComponent != null)
+                {
+                    originalIntensities[i] = lightComponent.intensity;
+                }
+            }
+        }
+    }
+
+    protected override void OnSelectEntered(SelectEnterEventArgs args)
+    {
+        base.OnSelectEntered(args);
+        ToggleLights();
+        
+    }
+    
+    protected override void OnHoverEntered(HoverEnterEventArgs args)
+    {
+        base.OnHoverEntered(args);
+        
+        if (args.interactorObject is UnityEngine.XR.Interaction.Toolkit.Interactors.XRDirectInteractor)
+        {
+            ToggleLights();
+        }
+    }
+
+    private void ToggleLights()
+    {
+        isOn = !isOn;
+
+        for (int i = 0; i < lights.Length; i++)
+        {
+            if (lights[i] != null)
+            {
+                Light lightComponent = lights[i].GetComponent<Light>();
+                if (lightComponent != null)
+                {
+                    if (isOn)
+                    {
+                        lightComponent.intensity = originalIntensities[i];
+                    }
+                    else
+                    {
+                        lightComponent.intensity = dimmedLight;
+                    }
+                }
+            }
+
+        }
+        
+        foreach (GameObject text in appearingTexts)
+        {
+            if(text != null) text.SetActive(!isOn);
+        }
+        
+        StopAllCoroutines(); 
+        StartCoroutine(ButtonPressAnimation());
+    }
+    
+    private IEnumerator ButtonPressAnimation()
+    {
+        transform.localPosition = originalPosition + new Vector3(0, 0, pushDistance);
+        
+        yield return new WaitForSeconds(0.15f);
+        
+        transform.localPosition = originalPosition;
+    }
+}
