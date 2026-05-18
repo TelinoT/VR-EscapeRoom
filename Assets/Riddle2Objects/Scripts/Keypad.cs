7f6753df6ba2f884833eb8214e08d959be390237
TelinoT <elias.piersig@icloud.com>
2026-05-18 16:05:24 +0200

    Planet Fix + Model Import
    - fixed the planets for the planet riddle
    - imported and placed a bunch of models

--- Assets/Riddle2Objects/Scripts/Keypad.cs
+++ Assets/Riddle2Objects/Scripts/Keypad.cs
@@ -11,10 +11,17 @@ public class Keypad : MonoBehaviour
 
     public UnityEvent onCorrect;
     public UnityEvent onWrong;
+    
+    public bool isGreek = false;
 
     public void ButtonPressed(string value)
     {
-        currentInput += value;
+		if(isGreek){
+			value = ConvertToGreekLetter(value);
+		}	
+
+        currentInput += value;	
+
         displayText.text = currentInput;
 
         if (currentInput.Length == correctCode.Length)
@@ -35,4 +42,22 @@ public class Keypad : MonoBehaviour
     {
         Debug.Log("Incorrect code");
     }
+
+	public string ConvertToGreekLetter(string inputNumber)
+	{
+    	return inputNumber switch
+    	{
+        	"1" => "α",
+       	 	"2" => "δ",
+        	"3" => "θ",
+        	"4" => "λ",
+        	"5" => "ξ",
+        	"6" => "π",
+        	"7" => "σ",
+        	"8" => "φ",
+        	"9" => "ψ",
+        	"0" => "ω",
+        	_ => inputNumber
+    	};
+	}
 }
